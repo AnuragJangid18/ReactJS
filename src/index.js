162adda7f572b56eb0dd72d5da5a21924e0f8710
AnuragJangid18 <anurag.jangid1811@gmail.com>
2026-03-30 17:32:06 +0530

    Add json-server as a dev dependency and simplify routing in index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // `react-dom` was updated in React 18, so use `react-dom/client`
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <Router>
-      <Routes>
-        {/* Define route using element */}
-        <Route path="/" element={<App />} />
-      </Routes>
+      <App />
     </Router>
   </React.StrictMode>
 );
